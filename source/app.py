f852718e7982c9765ac8e2f71dfffad15baa19f4
Michael Frikke Madsen <mibias.madsen@gmail.com>
2019-06-11 21:30:36 +0200

    Add k8s deployment and update app with ip page

--- source/app.py
+++ source/app.py
@@ -1,9 +1,18 @@
+import os
+import socket
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/ip")
+def ip():
+    hostname = socket.gethostname()
+    hello_target = os.environ.get('HELLO_TARGET', 'World')
+    return 'Hello, {}, from {}!\n'.format(hello_target, hostname)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
